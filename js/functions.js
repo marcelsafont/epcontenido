34ce1ffadb3976a96c89d053826403b95659b397
marcelsafont <marcel.safont1983@gmail.com>
2019-10-10 18:01:46 +0200

    added hide and show buttons

--- js/functions.js
+++ js/functions.js
@@ -10,6 +10,23 @@ $(window).load(function() {
 });
 
 $(document).ready(function(){
+	$('#colaborador-btn').click(function(){
+		$('#responsable').hide();
+		$('#colaborador').show(100, function(){
+			let pos = $('#colaborador .box-info-no-image').position();
+			window.scrollTo(0, pos.top);
+		});
+		
+	})
+
+	$('#responsable-btn').click(function(){
+		$('#colaborador').hide();
+		$('#responsable').show(100, function(){
+			let pos = $('#responsable .box-info-no-image').position();
+			window.scrollTo(0, pos.top);
+		});
+	})	
+
 	$('.box-checkbox').click(function(){
 		$(this).toggleClass('checked');
 	})
